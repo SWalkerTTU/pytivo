2c3c13e4c9d11713d0a89925d1798c3d1c47a865
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-25 13:16:21 -0800

    Add typing.

--- plugins/togo/togo.py
+++ plugins/togo/togo.py
@@ -6,16 +6,20 @@ import subprocess
 import sys
 import _thread
 import time
+from typing import TYPE_CHECKING, Any, Dict, List, BinaryIO
 import urllib.request, urllib.error, urllib.parse
 import urllib.parse
 from urllib.parse import quote, unquote
-from xml.dom import minidom
+from xml.dom import minidom  # type: ignore
 
 from Cheetah.Template import Template  # type: ignore
 
 import config
 import metadata
 from plugin import Plugin
+
+if TYPE_CHECKING:
+    from httpserver import TivoHTTPHandler
 
 logger = logging.getLogger("pyTivo.togo")
 tag_data = metadata.tag_data
@@ -68,16 +72,18 @@ tnname = os.path.join(SCRIPTDIR, "templates", "npl.tmpl")
 with open(tnname, "rb") as tnname_fh:
     NPL_TEMPLATE = tnname_fh.read()
 
-mswindows = sys.platform == "win32"
+MSWINDOWS = sys.platform == "win32"
 
-status = {}  # Global variable to control download threads
-tivo_cache = {}  # Cache of TiVo NPL
-queue = {}  # Recordings to download -- list per TiVo
-basic_meta = {}  # Data from NPL, parsed, indexed by progam URL
-details_urls = {}  # URLs for extended data, indexed by main URL
+STATUS: Dict[str, Dict[str, Any]] = {}  # Global variable to control download threads
+TIVO_CACHE: Dict[str, Dict[str, Any]] = {}  # Cache of TiVo NPL
+QUEUE: Dict[str, List[str]] = {}  # Recordings to download -- list per TiVo
+BASIC_META: Dict[
+    str, Dict[str, Any]
+] = {}  # Data from NPL, parsed, indexed by progam URL
+DETAILS_URLS: Dict[str, str] = {}  # URLs for extended data, indexed by main URL
 
 
-def null_cookie(name, value):
+def null_cookie(name: str, value: str) -> http.cookiejar.Cookie:
     return http.cookiejar.Cookie(
         0,
         name,
@@ -94,33 +100,42 @@ def null_cookie(name, value):
         False,
         None,
         None,
-        None,
+        {},
     )
 
 
-auth_handler = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+def getint(thing: Any) -> int:
+    try:
+        result = int(thing)
+    except:
+        result = 0
+    return result
+
+
+AUTH_HANDLER = urllib.request.HTTPPasswordMgrWithDefaultRealm()
 cj = http.cookiejar.CookieJar()
 cj.set_cookie(null_cookie("sid", "ADEADDA7EDEBAC1E"))
-tivo_opener = urllib.request.build_opener(
+TIVO_OPENER = urllib.request.build_opener(
     urllib.request.HTTPCookieProcessor(cj),
-    urllib.request.HTTPBasicAuthHandler(auth_handler),
-    urllib.request.HTTPDigestAuthHandler(auth_handler),
+    urllib.request.HTTPBasicAuthHandler(AUTH_HANDLER),
+    urllib.request.HTTPDigestAuthHandler(AUTH_HANDLER),
 )
 
-tsn = config.get_server("togo_tsn")
+
+tsn = config.get_server("togo_tsn", "")
 if tsn:
-    tivo_opener.addheaders.append(("TSN", tsn))
+    TIVO_OPENER.addheaders.append(("TSN", tsn))
 
 
 class ToGo(Plugin):
     CONTENT_TYPE = "text/html"
 
-    def tivo_open(self, url):
+    def tivo_open(self, url: str) -> BinaryIO:
         # Loop just in case we get a server busy message
         while True:
             try:
                 # Open the URL using our authentication/cookie opener
-                return tivo_opener.open(url)
+                return TIVO_OPENER.open(url)
 
             # Do a retry if the TiVo responds that the server is busy
             except urllib.error.HTTPError as e:
@@ -132,16 +147,9 @@ class ToGo(Plugin):
                 logger.error(e)
                 raise
 
-    def NPL(self, handler, query):
-        def getint(thing):
-            try:
-                result = int(thing)
-            except:
-                result = 0
-            return result
-
-        global basic_meta
-        global details_urls
+    def NPL(self, handler: "TivoHTTPHandler", query: Dict[str, Any]) -> None:
+        global BASIC_META
+        global DETAILS_URLS
         shows_per_page = 50  # Change this to alter the number of shows returned
         folder = ""
         FirstAnchor = ""
@@ -169,27 +177,27 @@ class ToGo(Plugin):
                 theurl += "&AnchorOffset=" + query["AnchorOffset"][0]
 
             if (
-                theurl not in tivo_cache
-                or (time.time() - tivo_cache[theurl]["thepage_time"]) >= 60
+                theurl not in TIVO_CACHE
+                or (time.time() - TIVO_CACHE[theurl]["thepage_time"]) >= 60
             ):
                 # if page is not cached or old then retreive it
-                auth_handler.add_password("TiVo DVR", ip_port, "tivo", tivo_mak)
+                AUTH_HANDLER.add_password("TiVo DVR", ip_port, "tivo", tivo_mak)
                 try:
                     page = self.tivo_open(theurl)
                 except IOError as e:
                     handler.redir(UNABLE % (tivoIP, cgi.escape(str(e))), 10)
                     return
-                tivo_cache[theurl] = {
+                TIVO_CACHE[theurl] = {
                     "thepage": minidom.parse(page),
                     "thepage_time": time.time(),
                 }
                 page.close()
 
-            xmldoc = tivo_cache[theurl]["thepage"]
+            xmldoc = TIVO_CACHE[theurl]["thepage"]
             items = xmldoc.getElementsByTagName("Item")
-            TotalItems = tag_data(xmldoc, "TiVoContainer/Details/TotalItems")
-            ItemStart = tag_data(xmldoc, "TiVoContainer/ItemStart")
-            ItemCount = tag_data(xmldoc, "TiVoContainer/ItemCount")
+            TotalItems = int(tag_data(xmldoc, "TiVoContainer/Details/TotalItems"))
+            ItemStart = int(tag_data(xmldoc, "TiVoContainer/ItemStart"))
+            ItemCount = int(tag_data(xmldoc, "TiVoContainer/ItemCount"))
             title = tag_data(xmldoc, "TiVoContainer/Details/Title")
             if items:
                 FirstAnchor = tag_data(items[0], "Links/Content/Url")
@@ -244,14 +252,14 @@ class ToGo(Plugin):
 
                     url = urllib.parse.urljoin(baseurl, entry["Url"])
                     entry["Url"] = url
-                    if url in basic_meta:
-                        entry.update(basic_meta[url])
+                    if url in BASIC_META:
+                        entry.update(BASIC_META[url])
                     else:
                         basic_data = metadata.from_container(item)
                         entry.update(basic_data)
-                        basic_meta[url] = basic_data
+                        BASIC_META[url] = basic_data
                         if "Details" in entry:
-                            details_urls[url] = entry["Details"]
+                            DETAILS_URLS[url] = entry["Details"]
 
                 data.append(entry)
         else:
@@ -265,9 +273,9 @@ class ToGo(Plugin):
         t = Template(NPL_TEMPLATE)
         t.quote = quote
         t.folder = folder
-        t.status = status
-        if tivoIP in queue:
-            t.queue = queue[tivoIP]
+        t.status = STATUS
+        if tivoIP in QUEUE:
+            t.queue = QUEUE[tivoIP]
         t.has_tivodecode = has_tivodecode
         t.togo_mpegts = config.is_ts_capable(tsn)
         t.tname = tivo_name
@@ -283,9 +291,9 @@ class ToGo(Plugin):
         t.title = title
         handler.send_html(str(t), refresh="300")
 
-    def get_tivo_file(self, tivoIP, url, mak, togo_path):
-        # global status
-        status[url].update({"running": True, "queued": False})
+    def get_tivo_file(self, tivoIP: str, url: str, mak: str, togo_path: str) -> None:
+        # global STATUS
+        STATUS[url].update({"running": True, "queued": False})
 
         parse_url = urllib.parse.urlparse(url)
 
@@ -295,8 +303,8 @@ class ToGo(Plugin):
             name.insert(-1, " - " + id)
         except:
             pass
-        ts = status[url]["ts_format"]
-        if status[url]["decode"]:
+        ts = STATUS[url]["ts_format"]
+        if STATUS[url]["decode"]:
             if ts:
                 name[-1] = "ts"
             else:
@@ -312,10 +320,10 @@ class ToGo(Plugin):
             name = name.replace(ch, BADCHAR[ch])
         outfile = os.path.join(togo_path, name)
 
-        if status[url]["save"]:
-            meta = basic_meta[url]
+        if STATUS[url]["save"]:
+            meta = BASIC_META[url]
             try:
-                handle = self.tivo_open(details_urls[url])
+                handle = self.tivo_open(DETAILS_URLS[url])
                 meta.update(metadata.from_details(handle.read()))
                 handle.close()
             except:
@@ -324,15 +332,15 @@ class ToGo(Plugin):
             metadata.dump(metafile, meta)
             metafile.close()
 
-        auth_handler.add_password("TiVo DVR", url, "tivo", mak)
+        AUTH_HANDLER.add_password("TiVo DVR", url, "tivo", mak)
         try:
-            if status[url]["ts_format"]:
+            if STATUS[url]["ts_format"]:
                 handle = self.tivo_open(url + "&Format=video/x-tivo-mpeg-ts")
             else:
                 handle = self.tivo_open(url)
         except Exception as msg:
-            status[url]["running"] = False
-            status[url]["error"] = str(msg)
+            STATUS[url]["running"] = False
+            STATUS[url]["error"] = str(msg)
             return
 
         tivo_name = config.tivos[config.tivos_by_ip(tivoIP)].get("name", tivoIP)
@@ -342,9 +350,9 @@ class ToGo(Plugin):
             % (time.strftime("%d/%b/%Y %H:%M:%S"), outfile, tivo_name)
         )
 
-        if status[url]["decode"]:
+        if STATUS[url]["decode"]:
             fname = outfile
-            if mswindows:
+            if MSWINDOWS:
                 fname = fname.encode("cp1252")
             tivodecode_path = config.get_bin("tivodecode")
             tcmd = [tivodecode_path, "-m", mak, "-o", fname, "-"]
@@ -359,7 +367,7 @@ class ToGo(Plugin):
         last_interval = start_time
         now = start_time
         try:
-            while status[url]["running"]:
+            while STATUS[url]["running"]:
                 output = handle.read(1024000)
                 if not output:
                     break
@@ -368,51 +376,51 @@ class ToGo(Plugin):
                 now = time.time()
                 elapsed = now - last_interval
                 if elapsed >= 5:
-                    status[url]["rate"] = "%.2f Mb/s" % (
+                    STATUS[url]["rate"] = "%.2f Mb/s" % (
                         length * 8.0 / (elapsed * 1024 * 1024)
                     )
-                    status[url]["size"] += length
+                    STATUS[url]["size"] += length
                     length = 0
                     last_interval = now
-            if status[url]["running"]:
-                status[url]["finished"] = True
+            if STATUS[url]["running"]:
+                STATUS[url]["finished"] = True
         except Exception as msg:
-            status[url]["running"] = False
+            STATUS[url]["running"] = False
             logger.info(msg)
         handle.close()
         f.close()
-        status[url]["size"] += length
-        if status[url]["running"]:
+        STATUS[url]["size"] += length
+        if STATUS[url]["running"]:
             mega_elapsed = (now - start_time) * 1024 * 1024
             if mega_elapsed < 1:
                 mega_elapsed = 1
-            size = status[url]["size"]
+            size = STATUS[url]["size"]
             rate = size * 8.0 / mega_elapsed
             logger.info(
                 '[%s] Done getting "%s" from %s, %d bytes, %.2f Mb/s'
                 % (time.strftime("%d/%b/%Y %H:%M:%S"), outfile, tivo_name, size, rate)
             )
-            status[url]["running"] = False
+            STATUS[url]["running"] = False
         else:
             os.remove(outfile)
-            if status[url]["save"]:
+            if STATUS[url]["save"]:
                 os.remove(outfile + ".txt")
             logger.info(
                 '[%s] Transfer of "%s" from %s aborted'
                 % (time.strftime("%d/%b/%Y %H:%M:%S"), outfile, tivo_name)
             )
-            del status[url]
+            del STATUS[url]
 
-    def process_queue(self, tivoIP, mak, togo_path):
-        while queue[tivoIP]:
+    def process_queue(self, tivoIP: str, mak: str, togo_path: str) -> None:
+        while QUEUE[tivoIP]:
             time.sleep(5)
-            url = queue[tivoIP][0]
+            url = QUEUE[tivoIP][0]
             self.get_tivo_file(tivoIP, url, mak, togo_path)
-            queue[tivoIP].pop(0)
-        del queue[tivoIP]
+            QUEUE[tivoIP].pop(0)
+        del QUEUE[tivoIP]
 
-    def ToGo(self, handler, query):
-        togo_path = config.get_server("togo_path")
+    def ToGo(self, handler: "TivoHTTPHandler", query: Dict[str, Any]) -> None:
+        togo_path = config.get_server("togo_path", "")
         for name, data in config.getShares():
             if togo_path == name:
                 togo_path = data.get("path")
@@ -425,7 +433,7 @@ class ToGo(Plugin):
             save = "save" in query
             ts_format = "ts_format" in query
             for theurl in urls:
-                status[theurl] = {
+                STATUS[theurl] = {
                     "running": False,
                     "error": "",
                     "rate": "",
@@ -436,10 +444,10 @@ class ToGo(Plugin):
                     "save": save,
                     "ts_format": ts_format,
                 }
-                if tivoIP in queue:
-                    queue[tivoIP].append(theurl)
+                if tivoIP in QUEUE:
+                    QUEUE[tivoIP].append(theurl)
                 else:
-                    queue[tivoIP] = [theurl]
+                    QUEUE[tivoIP] = [theurl]
                     _thread.start_new_thread(
                         ToGo.process_queue, (self, tivoIP, tivo_mak, togo_path)
                     )
@@ -453,16 +461,16 @@ class ToGo(Plugin):
             message = MISSING
         handler.redir(message, 5)
 
-    def ToGoStop(self, handler, query):
+    def ToGoStop(self, handler: "TivoHTTPHandler", query: Dict[str, Any]) -> None:
         theurl = query["Url"][0]
-        status[theurl]["running"] = False
+        STATUS[theurl]["running"] = False
         handler.redir(TRANS_STOP % unquote(theurl))
 
-    def Unqueue(self, handler, query):
+    def Unqueue(self, handler: "TivoHTTPHandler", query: Dict[str, Any]) -> None:
         theurl = query["Url"][0]
         tivoIP = query["TiVo"][0]
-        del status[theurl]
-        queue[tivoIP].remove(theurl)
+        del STATUS[theurl]
+        QUEUE[tivoIP].remove(theurl)
         logger.info(
             '[%s] Removed "%s" from queue'
             % (time.strftime("%d/%b/%Y %H:%M:%S"), unquote(theurl))
