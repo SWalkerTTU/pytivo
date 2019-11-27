9eca773342a77f1a3911ece5d01c2b6a71625df3
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-27 12:08:44 -0800

    Remove debug prints.

--- plugins/music/music.py
+++ plugins/music/music.py
@@ -540,13 +540,11 @@ class Music(Plugin):
             filelist.unsorted = False
 
         files = filelist.files[:]
-        print([x.name for file in files])
 
         # Trim the list
         files, total, start_item = self.item_count(
             handler, query, handler.cname, files, filelist.last_start
         )
-        print([x.name for file in files])
         filelist.last_start = start_item
         return files, total, start_item
 
