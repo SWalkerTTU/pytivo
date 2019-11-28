528f439f2ed061ae394945096195661c271a278b
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-27 18:11:30 -0800

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# setup for datadiary
+
+# import os.path
+from setuptools import setup, find_packages
+
+# here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="pytivo",
+    version="0.1.0",
+    description=(
+        "TiVo HMO and GoBack server.  Used to serve videos and other media "
+        "to a TiVo from a computer."
+    ),
+    author="Matthew A. Clapp",
+    author_email="itsayellow+dev@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="tivo",
+    url="https://github.com/itsayellow/pytivo",
+    include_package_data=True,  # to get templates and images
+    packages=["src/pytivo"],
+    install_requires=["mutagen", "Cheetah3", "zeroconf", "Pillow"],
+    entry_points={"console_scripts": ["pytivo=pytivo.pyTivo:cli"]},
+    python_requires=">=3.6",
+)
