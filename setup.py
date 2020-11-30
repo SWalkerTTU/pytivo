768ebb0cf41b7a1425e9596d495d99470e2433b5
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-29 17:39:27 -0800

    Switch to PEP517 project.

--- setup.py
+++ setup.py
@@ -1,46 +1,7 @@
-# setup for datadiary
+#!/usr/bin/env python3
 
 import sys
-from setuptools import setup, find_packages
 
-# here = os.path.abspath(os.path.dirname(__file__))
+from setuptools import setup  # type: ignore
 
-# extra windows-only executable
-if sys.platform == "win32":
-    extra_console_scripts = ["pytivoservice=pytivo.pyTivoService:cli"]
-else:
-    extra_console_scripts = []
-
-setup(
-    name="pytivo",
-    version="0.2.0",
-    description=(
-        "TiVo HMO and GoBack server.  Used to serve videos and other media "
-        "to a TiVo from a computer."
-    ),
-    author="Matthew A. Clapp",
-    author_email="itsayellow+dev@gmail.com",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
-    ],
-    keywords="tivo",
-    url="https://github.com/itsayellow/pytivo",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    package_data={
-        # in any package, include template dirs and any files within
-        "": ["templates/*", "help.txt"]
-    },
-    install_requires=[
-        "mutagen",
-        "Cheetah3",
-        "zeroconf>0.23.0",
-        "Pillow",
-        "pywin32;platform_system=='Windows'",
-    ],
-    entry_points={
-        "console_scripts": ["pytivo=pytivo.main:cli"] + extra_console_scripts
-    },
-    python_requires=">=3.6",
-)
+setup()
